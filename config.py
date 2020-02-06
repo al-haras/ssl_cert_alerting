af5fbd6110379583d69e3ac8e69223cd3edb3225
al-haras <alex_haras@yahoo.com>
2020-02-05 20:19:41 -0800

    remove files

--- config.py
+++
@@ -1,7 +0,0 @@
-smtp_server = 'smtp.mydomain.com'
-smtp_port = 25
-password = 'mypassword'
-sender_email = 'sender@email.com'
-receiver_email = 'receiver@email.com'
-slack_oauth_token = 'my-slack-oauth'
-slack_channel = 'my-slack-channel'
